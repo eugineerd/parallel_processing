75f40eef802f1cf372b7be2ee0592fcd8c4eb281
Eugene <evd.jeka@gmail.com>
2022-12-09 15:42:41 +0000

    draw better plots

--- pthreads/lab1/run_tests.py
+++ pthreads/lab1/run_tests.py
@@ -4,47 +4,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 times = []
-sizes =[(4, 1000), (10, 1000), (20, 1000), (50, 1000), (100, 500), (250,100), (1000, 10)] 
-plt.rcParams["figure.figsize"] = (30,30)
-fig, ax = plt.subplots(3, 4)
+sizes =[(4, 200), (10, 200), (20, 200), (50, 200), (100, 200), (250, 20), (1000, 3)] 
+threads = [1, 2, 4, 8, 16, 32]
+plt.rcParams["figure.figsize"] = (30,50)
+fig, ax = plt.subplots(3, len(sizes))
 
-l = 0
-alg = "row_mult"
-for i, t in enumerate([1, 2, 4, 8]):
-    times = []
-    for m, r in sizes:
-        s = 0
-        for j in tqdm.tqdm(range(r)):
-            proc = run([f"lab1/{alg}", f"../lab1/data/{m}x{m}.in", "/dev/null", f"{t}"], stdout=PIPE)
-            s += int(proc.stdout.decode("utf-8"))
-        time = s / r / 1000
-        out = f"{alg} {m}x{m} {t}: {time:.3f}ms\n"
-        with open("results_.txt", "a+") as ff:
-            ff.write(out)
-        print(out, end="")
-        times.append(time)
-    ax[l][i].set_title(f"{alg}, {t} threads")
-    ax[l][i].set_xlabel("matrix size")
-    ax[l][i].set_yscale("log")
-    ax[l][i].set_ylabel("time, ms")
-    sns.lineplot(y=times, x=[f"{x[0]}x{x[0]}" for x in sizes], ax=ax[l][i])
-
-col_sizes = sizes[:5]
-times = []
-for m, r in col_sizes:
-    s = 0
-    f = open(f"../lab1/data/{m}x{m}.in")
-    for j in tqdm.tqdm(range(r)):
-        proc = run([f"lab1/col_mult"], stdout=PIPE, stdin=f)
-        s += int(proc.stdout.decode("utf-8"))
-    f.close()
-    time = s / r / 1000
-    print(f"col_mult: {time:.3f}ms")
-    times.append(time)
-ax[1][0].set_title(f"col_mult")
-ax[1][0].set_xlabel("matrix size")
-ax[1][0].set_yscale("log")
-ax[1][0].set_ylabel("time, ms")
-sns.lineplot(y=times, x=[f"{x[0]}x{x[0]}" for x in col_sizes], ax=ax[1][0])
-
+for l, alg in enumerate(["col_mult", "row_mult"]):
+    for size_n, (m, r) in enumerate(sizes):
+        times = []
+        for i, t in enumerate(threads):
+            s = 0
+            out = f"{alg} {m}x{m} {t}"
+            for j in tqdm.tqdm(range(r), desc=out):
+                proc = run([f"lab1/{alg}", f"../lab1/data/{m}x{m}.in", "/dev/null", f"{t}"], stdout=PIPE)
+                s += int(proc.stdout.decode("utf-8"))
+            time = s / r / 1000
+            out += f": {time:.3f}ms\n"
+            with open("results_.txt", "a+") as ff:
+                ff.write(out)
+            print(out, end="")
+            times.append(time)
+        ax[l][size_n].set_title(f"{alg}, {m}x{m} matrix")
+        ax[l][size_n].set_xlabel("num threads")
+        ax[l][size_n].set_yscale("log")
+        ax[l][size_n].set_ylabel("time, ms")
+        sns.lineplot(y=times, x=[f"{x} threads" for x in threads], ax=ax[l][size_n])
 fig.savefig("out.png", bbox_inches='tight')
